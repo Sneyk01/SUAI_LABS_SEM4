18b34969747e38a6bf2908f92197ac2bb131a922
Sneyk01 <90918010+Sneyk01@users.noreply.github.com>
2023-03-30 18:44:44 +0300

    Add files via upload

---
+++ lab1/lab1_console_part2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace lab1_console_part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string num = "";
+
+            while (num == "")
+            {
+                Console.WriteLine("Введите натуральное число");
+                num = Console.ReadLine();
+
+                if (num != "")
+                {
+                    foreach (char s in num)
+                    {
+                        if (!char.IsNumber(s))
+                        {
+                            Console.WriteLine("Некорректное число");
+                            num = "";
+                            break;
+                        }
+                    }
+                }
+            }
+
+            
+
+            string k = "";
+
+            while (k == "")
+            {
+                Console.WriteLine("Введите число К");
+                k = Console.ReadLine();
+
+                if (k != "")
+                {
+                    if (!(k[0] == '-' || char.IsNumber(k[0])))
+                    {
+                        k = "";
+                        break;
+                    }
+                        
+
+                    for (int i = 1; i < k.Length; i++)
+                    {
+                        if (!char.IsNumber(k[i]))
+                        {
+                            Console.WriteLine("Некорректное число");
+                            k = "";
+                            break;
+                        }
+                    }
+                }
+            }
+
+            
+
+            int count = 1;
+            int num1 = Int32.Parse(num);
+            int num2 = Int32.Parse(k);
+
+            for (int i = (num2 + 1); i < num1; i++)
+            {
+                if (i != 0)
+                    if ((num1 % i) == 0)
+                        count++;
+            }
+
+            Console.WriteLine($"Число делителей числа {num}, больших чем {k}: {count.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab1/lab1_console_part3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab1_console_part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "a^2 * b = c \r\n";
+
+            for (int a = 1; a < 21; a++)
+            {
+                if (a * a > 20)
+                    break;
+
+                for (int b = 1; b < 21; b++)
+                {
+                    if (a * a * b > 20)
+                        break;
+
+                    if (a * a * b > 0 && a * a * b < 21)
+                        answer += $"{a}^2 * {b} = {a * a * b} \r\n";
+                }
+            }
+
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+    }
+}
